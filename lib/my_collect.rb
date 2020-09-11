0f26a5197ad193a44123d8f3307ab047a6868549
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-11 12:38:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,9 @@ names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 def my_collect(array)
   i = 0
   new_array = []
-  
+  while i < array.length 
+    new_array << yield(array[i])
+  end
   
 end
 
